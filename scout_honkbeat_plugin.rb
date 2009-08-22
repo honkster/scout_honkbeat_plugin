2a420d4b31f07119db0170ce6c1e436e8a8abbda
Mason Glaves & Mike Hagedorn <pair-mason.glaves-and-mike.hagedorn@pivotallabs.com>
2009-08-21 17:04:20 -0700

    finishing up plugin

--- scout_honkbeat_plugin.rb
+++ scout_honkbeat_plugin.rb
@@ -2,33 +2,44 @@
 require 'json'
 
 class ScoutHonkbeatPlugin < Scout::Plugin
+  
   def build_report
+    
     host = `hostname`.strip
     machine_file_path = "/data/honk/shared/status/#{host}/machine_status.txt"
     external_dependencies_file_path = "/data/honk/shared/status/#{host}/dependency_status.txt"
 
-    machine_status = JSON.parse(File.read(machine_file_path))
-    down = []
-    machine_status.each do |error|
-      down << "#{error[:hostname]}:#{error[:port]}"
+    machine_status = JSON.parse(File.read(machine_file_path))[0]
+    external_status = JSON.parse(File.read(external_dependencies_file_path))
+
+    down = machine_status.map do |error|
+      "#{error['hostname']}:#{error['port']}"
     end
 
     last_down = memory(:down)
-    now_up = last_down - down
     remember(:down, down)
+
+    now_up   = []
+    now_up   = last_down - down if last_down
+    down     = down - last_down if last_down
+
+    #shouldnt fire on empty... but is
     alert("The following servers are now UP   : #{now_up.join(', ')}") unless now_up.empty?
     alert("The following servers are now DOWN : #{down.join(', ')}") unless down.empty?
 
-     external_status = JSON.parse(File.read(external_dependencies_file_path))
-    down_external = []
-    last_external_down = memory(:down_external)
-    external_status.each do |error|
-      down_external << "#{error[:name]}:#{error[:type]}"
+    down_external = external_status.map do |error|
+      "#{error['name']}:#{error['type']}"
     end
-    now_up_external = last_external_down - down_external
+
+    last_down_external = memory(:down_external)
+    remember(:down_external, down_external)
+
+    now_up_external = []
+    now_up_external = last_down_external - down_external if last_down_external
+    down_external   = down_external - last_down_external if last_down_external
+
     alert("The following external dependencies are now UP   : #{now_up_external.join(', ')}") unless now_up_external.empty?
     alert("The following external dependencies are now DOWN : #{down_external.join(', ')}") unless down_external.empty?
-
-    remember(:down_external, down_external)
+  
   end
 end
