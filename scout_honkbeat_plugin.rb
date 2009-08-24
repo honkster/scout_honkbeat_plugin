6c099c3c837c46647939ef097283b25c049425e6
Brian Takita & Mike Hagedorn <pair-brian.takita-and-mike.hagedorn@pivotallabs.com>
2009-08-24 14:35:52 -0700

    Added report data. Extracted methods in plugin. Ignoring rubymine files.

--- scout_honkbeat_plugin.rb
+++ scout_honkbeat_plugin.rb
@@ -2,20 +2,27 @@
 require 'json'
 
 class ScoutHonkbeatPlugin < Scout::Plugin
-  
+
   def build_report
-    
-    host = `hostname`.strip
-    machine_file_path = "/data/honk/shared/status/#{host}/machine_status.txt"
-    external_dependencies_file_path = "/data/honk/shared/status/#{host}/dependency_status.txt"
-
-    unless File.exists?(machine_file_path) && File.exists?(external_dependencies_file_path)
+    if report_files_exist?
+      process_report_files
+    else
       alert("There are no status files available")
-      return
     end
-    machine_status = JSON.parse(File.read(machine_file_path))[0]
-    external_status = JSON.parse(File.read(external_dependencies_file_path))
+  end
 
+  def process_report_files
+    check_server_health
+    check_external_services
+    report(report_data)
+  end
+
+  def report_files_exist?
+    File.exists?(machine_file_path) && File.exists?(external_dependencies_file_path)
+  end
+
+  def check_server_health
+    machine_status = JSON.parse(File.read(machine_file_path))[0]
     down = machine_status.map do |error|
       "#{error['hostname']}:#{error['port']}"
     end
@@ -27,9 +34,13 @@ class ScoutHonkbeatPlugin < Scout::Plugin
     now_up   = last_down - down if last_down
     down     = down - last_down if last_down
 
-    #shouldnt fire on empty... but is
+    report_data["Server Errors"] = down.join(', ')
     alert("The following servers are now UP   : #{now_up.join(', ')}") unless now_up.empty?
     alert("The following servers are now DOWN : #{down.join(', ')}") unless down.empty?
+  end
+
+  def check_external_services
+    external_status = JSON.parse(File.read(external_dependencies_file_path))
 
     down_external = external_status.map do |error|
       "#{error['name']}:#{error['type']}"
@@ -42,8 +53,24 @@ class ScoutHonkbeatPlugin < Scout::Plugin
     now_up_external = last_down_external - down_external if last_down_external
     down_external   = down_external - last_down_external if last_down_external
 
+    report_data["External Service Errors"] = down_external.join(', ')
     alert("The following external dependencies are now UP   : #{now_up_external.join(', ')}") unless now_up_external.empty?
     alert("The following external dependencies are now DOWN : #{down_external.join(', ')}") unless down_external.empty?
-  
+  end
+
+  def host
+    @host ||= `hostname`.strip
+  end
+
+  def machine_file_path
+    "/data/honk/shared/status/#{host}/machine_status.txt"
+  end
+
+  def external_dependencies_file_path
+    "/data/honk/shared/status/#{host}/dependency_status.txt"
+  end
+
+  def report_data
+    @report_data ||= {}
   end
 end
