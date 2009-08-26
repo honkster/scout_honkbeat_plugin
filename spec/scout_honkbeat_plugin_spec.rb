dd0f738246d78ca172847365bb14908dafe02aeb
Andrew Cantino & Brian Takita <pair-andrew-and-brian@honk.com>
2009-08-26 11:41:19 -0700

    Refactored tests.

--- spec/scout_honkbeat_plugin_spec.rb
+++ spec/scout_honkbeat_plugin_spec.rb
@@ -18,97 +18,123 @@ describe ScoutHonkbeatPlugin do
   end
 
   describe "#build_report" do
-    describe "mongrels" do
-
-      context "when the machine status file does not exist" do
-        def self.should_alert_and_remember_current_time
-          it "sends an alert about the missing machine status file" do
-            plugin.build_report
-            plugin.alerts.should include("Error: machine_status.txt is missing")
-          end
-
-          it "remembers the current Time" do
-            now = Time.now
-            stub(Time).now {now}
-            mock(plugin).remember(:last_missing_file_alert_sent_at, now)
-
-            plugin.build_report
-          end
-        end
-
-        def self.should_not_alert_and_should_remember_the_old_time
-          it "does not send an alert about the missing machine status file" do
-            plugin.build_report
-            plugin.alerts.should_not include("Error: machine_status.txt is missing")
-          end
-
-          it "remembers the existing :last_missing_file_alert_sent_at" do
-            mock(plugin).remember(:last_missing_file_alert_sent_at, last_missing_file_alert_sent_at)
-            plugin.build_report
-          end
-        end
-        
+    def self.should_periodically_send_error_alerts (memory_item_name, error_message)
+      context "when memory(#{memory_item_name.inspect}) is nil" do
         before do
-          File.exists?(machine_status_path).should be_false
+          mock(plugin).memory(memory_item_name) {nil}
         end
 
-        context "when memory(:no_status_files_at) is nil" do
-          before do
-            mock(plugin).memory(:last_missing_file_alert_sent_at) {nil}
-          end
-
-          should_alert_and_remember_current_time
+        it "sends an alert about the error" do
+          plugin.build_report
+          plugin.alerts.should include(error_message)
         end
 
-        context "when memory(:last_missing_file_alert_sent_at) is within the last ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes" do
-          attr_reader :last_missing_file_alert_sent_at
-          before do
-            @last_missing_file_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES - 1) * 60)
-            mock(plugin).memory(:last_missing_file_alert_sent_at) { last_missing_file_alert_sent_at }
-          end
+        it "remembers the current Time" do
+          now = Time.now
+          stub(Time).now {now}
+          mock(plugin).remember(memory_item_name, now)
 
-          should_not_alert_and_should_remember_the_old_time
-        end
-
-        context "when memory(:last_missing_file_alert_sent_at) is more than ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes ago" do
-          attr_reader :last_missing_file_alert_sent_at
-          before do
-            @last_missing_file_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES + 1) * 60)
-            mock(plugin).memory(:last_missing_file_alert_sent_at) { last_missing_file_alert_sent_at }
-          end
-          
-          should_alert_and_remember_current_time
+          plugin.build_report
         end
       end
 
-      context "when the machine status file exists" do
+      context "when memory(#{memory_item_name.inspect}) is within the last ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes" do
+        attr_reader :last_alert_sent_at
         before do
-          File.open(machine_status_path, "w") do |file|
-            file.write([].to_json)
-          end
-          File.exists?(machine_status_path).should be_true
+          @last_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES - 1) * 60)
+          mock(plugin).memory(memory_item_name) { last_alert_sent_at }
         end
 
+        it "does not send an alert about the error" do
+          plugin.build_report
+          plugin.alerts.should_not include(error_message)
+        end
+
+        it "remembers the existing memory_item_name" do
+          mock(plugin).remember(memory_item_name, last_alert_sent_at)
+          plugin.build_report
+        end
+      end
+
+      context "when memory(#{memory_item_name.inspect}) is more than ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes ago" do
+        attr_reader :last_alert_sent_at
+        before do
+          @last_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES + 1) * 60)
+          mock(plugin).memory(memory_item_name) { last_alert_sent_at }
+        end
+
+        it "sends an alert about the missing machine status file" do
+          plugin.build_report
+          plugin.alerts.should include(error_message)
+        end
+
+        it "remembers the current Time" do
+          now = Time.now
+          stub(Time).now {now}
+          mock(plugin).remember(memory_item_name, now)
+
+          plugin.build_report
+        end
+      end
+    end
+
+    def self.should_send_success_alert(memory_item_name, success_message)
+      context "when #{memory_item_name.inspect} is not present" do
+        before do
+          mock(plugin).memory(memory_item_name) {nil}
+        end
+
+        it "should not remember #{memory_item_name.inspect}" do
+          dont_allow(plugin).remember(memory_item_name, anything)
+          plugin.build_report
+        end
+
+        it "should not send an alert indicating that the machine status file is back" do
+          plugin.build_report
+          plugin.alerts.should_not include(success_message)
+        end
+      end
+
+      context "when #{memory_item_name.inspect} is present" do
+        before do
+          mock(plugin).memory(memory_item_name) {Time.now}
+        end
+
+        it "should not remember #{memory_item_name.inspect}" do
+          dont_allow(plugin).remember(memory_item_name, anything)
+          plugin.build_report
+        end
+
+        it "should send an alert indicating that the machine status file is back" do
+          plugin.build_report
+          plugin.alerts.should include(success_message)
+        end
+      end
+    end
+
+    context "when the machine status file does not exist" do
+      before do
+        File.exists?(machine_status_path).should be_false
+      end
+
+      should_periodically_send_error_alerts(:last_missing_file_alert_sent_at, "Error: machine_status.txt is missing")
+    end
+
+    context "when the machine status file exists" do
+      before do
+        File.open(machine_status_path, "w") do |file|
+          file.write([].to_json)
+        end
+        File.exists?(machine_status_path).should be_true
+      end
+
+      describe "freshness of machine status file" do
         it "should not alert about a missing machine status file" do
           plugin.build_report
           plugin.alerts.should_not include("Error: machine_status.txt is missing")
         end
 
-        context "when :last_missing_file_alert_sent_at is present" do
-          before do
-            mock(plugin).memory(:last_missing_file_alert_sent_at) {Time.now}
-          end
-
-          it "should not remember :last_missing_file_alert_sent_at" do
-            dont_allow(plugin).remember(:last_missing_file_alert_sent_at, anything)
-            plugin.build_report
-          end
-
-          it "should send an alert indicating that the machine status file is back" do
-            plugin.build_report
-            plugin.alerts.should include("Success: machine_status.txt is back")
-          end
-        end
+        should_send_success_alert(:last_missing_file_alert_sent_at, "Success: machine_status.txt is back")
 
         context "when the machine status file is less than ScoutHonkbeatPlugin::STALE_FILE_THRESHOLD_IN_MINUTES minutes old" do
           before do
@@ -127,21 +153,7 @@ describe ScoutHonkbeatPlugin do
             end
           end
 
-          context "when :last_stale_file_alert_sent_at is set" do
-            before do
-              mock(plugin).memory(:last_stale_file_alert_sent_at) {Time.now}
-            end
-
-            it "should alert that the machine_status is no longer stale" do
-              plugin.build_report
-              plugin.alerts.should include("Success: machine_status.txt is no longer stale")
-            end
-
-            it "should not remember :last_stale_file_alert_sent_at" do
-              dont_allow(plugin).remember(:last_stale_file_alert_sent_at, anything)
-              plugin.build_report
-            end
-          end
+          should_send_success_alert(:last_stale_file_alert_sent_at, "Success: machine_status.txt is no longer stale")
         end
 
         context "when the machine status file is more than ScoutHonkbeatPlugin::STALE_FILE_THRESHOLD_IN_MINUTES minutes old" do
@@ -151,63 +163,13 @@ describe ScoutHonkbeatPlugin do
             File.exists?(machine_status_path).should be_true
           end
 
-          context "when :last_stale_file_alert_sent_at is nil" do
-            it "sends an alert about the stale machine status file" do
-              plugin.build_report
-              plugin.alerts.should include("Error: machine_status.txt is stale")
-            end
-
-            it "remembers Time.now" do
-              now = Time.now
-              stub(Time).now {now}
-              mock(plugin).remember(:last_stale_file_alert_sent_at, now)
-
-              plugin.build_report
-            end
-          end
-
-          context "when :last_stale_file_alert_sent_at less than ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes ago" do
-            attr_reader :last_stale_file_alert_sent_at
-            before do
-              @last_stale_file_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES - 1) * 60)
-              mock(plugin).memory(:last_stale_file_alert_sent_at) {last_stale_file_alert_sent_at}
-            end
-
-            it "does not send an alert about the stale machine status file" do
-              plugin.build_report
-              plugin.alerts.should_not include("Error: machine_status.txt is stale")
-            end
-
-            it "remembers the existing :last_stale_file_alert_sent_at" do
-              mock(plugin).remember(:last_stale_file_alert_sent_at, last_stale_file_alert_sent_at)
-              plugin.build_report
-            end
-          end
-
-          context "when :last_stale_file_alert_sent_at more than ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES minutes ago" do
-            attr_reader :last_stale_file_alert_sent_at
-            before do
-              @last_stale_file_alert_sent_at = Time.now - ((ScoutHonkbeatPlugin::ALERT_INTERVAL_IN_MINUTES + 1) * 60)
-              mock(plugin).memory(:last_stale_file_alert_sent_at) {last_stale_file_alert_sent_at}
-            end
-
-            it "sends an alert about the stale machine status file" do
-              plugin.build_report
-              plugin.alerts.should include("Error: machine_status.txt is stale")
-            end
-
-            it "remembers Time.now" do
-              now = Time.now
-              stub(Time).now {now}
-              mock(plugin).remember(:last_stale_file_alert_sent_at, now)
-
-              plugin.build_report
-            end
-          end
+          should_periodically_send_error_alerts(:last_stale_file_alert_sent_at, "Error: machine_status.txt is stale")
         end
+      end
 
+      describe "contents of machine status file" do
         context "there are new errors on mongrels on this machine" do
-          
+
         end
 
         context "there were mongrel errors on this machine, but they are gone now" do
@@ -218,7 +180,10 @@ describe ScoutHonkbeatPlugin do
 
         end
       end
+    end
 
+    def set_memory(key, value)
+      mock(plugin).memory(key) {value}
     end
   end
 end
